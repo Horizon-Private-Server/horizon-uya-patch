ebda8a65d3f0628c853dc35ef4385c60e57e6f24
Metroynome <yort124@gmail.com>
2021-12-06 23:00:56 -0500

    Updated Player Struct

--- libuya/include/player.h
+++ libuya/include/player.h
@@ -76,186 +76,35 @@ typedef struct CameraAngle
 typedef struct Player
 {
     struct Guber Guber;                                             // 0x00
-    char UNK0[0x88];                                                // 0x18
 
-    VECTOR PlayerPosition;                                          // 0xA0
+    char UNK0[0x328];
+
+    float WeaponCooldownTimer;                                      // 0x328
+
+    char UNK3[0xE58];
+
+    VECTOR CameraPos;                                               // 0x1180
+
+    char UNK1[0x50];
+
+    CameraAngle CameraYaw;                                          // 0x11E0
+    CameraAngle CameraPitch;                                        // 0x1200
+
+    char UNK4[0x54];
+
+    float CameraPitchMin;                                           // 0x1254
+    float CameraPitchMax;                                           // 0x1258
+
+    // In libdl this is: Moby * SkinMoby;
+    // Using int for now because moby.h is not impamented yet.
+    int SkinMoby;                                                   // 0x125C
+
+    float CameraDistance;                                           // 0x1260
+
+    char UNK5[0x12D8];
+
+    VECTOR PlayerPosition;                                          // 0x2538
     
-    char UNK1[0x04];                                                // 0xB0
-
-    float PlayerPitch;                                              // 0xB4
-    float PlayerYaw;                                                // 0xB8
-
-    char UNK2[0x240];                                               // 0xBC
-
-    int Airwalk;                                                    // 0x2FC
-
-    char UNK3[0x70];                                                // 0x300
-
-    // This resets whenever you jump/cboot/land
-    // Appears to count how long since you've entered a state
-    // Setting it to 0 when cbooting gives infinite cbooting
-    int TicksSinceStateChanged;                                     // 0x370
-    
-    // More state related ticks
-    int StateTicks[5];                                              // 0x374
-
-    char UNK4[0x28];                                                // 0x388
-
-
-    int RespawnTimer;                                               // 0x3B0
-
-    char UNK30[0x38];                                               // 0x3B4
-
-    float WeaponCooldownTimer;                                      // 0x3EC
-
-    char UNK5[0x18];                                                // 0x3F0
-
-    short ShieldTimer;                                              // 0x408
-    short QuadTimer;                                                // 0x40A
-    int UNK6;                                                       // 0x40C
-    short UNK7;                                                     // 0x410
-    short FireTimer;                                                // 0x412
-    short AcidTimer;                                                // 0x414
-    short FreezeTimer;                                              // 0x416
-    short UNK8;                                                     // 0x418
-    short JuggyElectricityTimer;                                    // 0x41A
-    short UNK9;                                                     // 0x41C
-    short BlowupTimer;                                              // 0x41E
-
-    char UNK10[0x3DC];                                              // 0x420
-
-    // First instance of weapon held id
-    // This one is zero for any item that isn't a gun
-    // So wrench and swingshot are 0
-    int WeaponHeldGun;                                              // 0x7FC
-
-    char UNK11[0x1150];                                             // 0x800
-
-    VECTOR CameraPos;                                               // 0x1950
-    char UNK99[0x50];                                               // 0x1960
-
-    CameraAngle CameraYaw;                                          // 0x19B0
-    CameraAngle CameraPitch;                                        // 0x19D0
-
-    char UNK13[0x0C];                                               // 0x19F0
-
-    // This is a pointer to the moby that the
-    // player is looking/aiming at
-    void * LookAtMoby;                                              // 0x19FC
-
-    char UNK12[0x20];                                               // 0x1A00
-
-    char UNK14[0x04];                                               // 0x1A20
-
-    float CameraPitchMin;                                           // 0x1A24
-    float CameraPitchMax;                                           // 0x1A28
-
-    void * SkinMoby;                                                // 0x1A2C
-
-    float CameraDistance;                                           // 0x1A30
-
-    char UNK15[0x1C];                                               // 0x1A34
-
-    short CameraType2;                                              // 0x1A50
-
-    char UNK45[0x4A];                                               // 0x1A52
-
-    int LocalPlayerIndex;                                           // 0x1A9C
-
-    char UNK16[0x8];                                                // 0x1AA0
-
-    int PlayerId;                                                   // 0x1AA8
-
-    char UNK33[0x34];                                               // 0x1AAC
-
-    int CameraType;                                                 // 0x1AE0
-
-    char UNK47[0x18];                                               // 0x1AE4
-
-    float CameraElevation;                                          // 0x1AFC
-
-    char UNK17[0x7F8];                                              // 0x1B00
-
-    // When 1, the player is trying to shoot
-    int IsShooting;                                                 // 0x22F8
-
-    char UNK29[0x18];                                               // 0x22FC
-
-    // When this value is set to 3,
-    // The weapon held is changed to the weapon id
-    // set in WeaponHeldId
-    int WeaponSwitchState;                                          // 0x2314
-    int WeaponSwitchLastWeaponHeldId;                               // 0x2318
-
-    char UNK28[0x194];                                              // 0x231C
-
-    int WeaponHeldId0;                                              // 0x24B0
-
-    char UNK18[0x118];                                              // 0x24B4
-
-    int PlayerState;                                                // 0x25CC
-
-    char UNK19[0x90];                                               // 0x25D0
-
-    // These seem to do a variety of things
-    // 0x04: 1 = Disables shooting and cbooting
-    // 0x06: 1 = Equips wrench
-    // 0x0C: 1 = Equips swingshot
-    // 0x0D: Toggles respawn popup if dead, otherwise just fucks camera
-    // 0x12: 1 = Disables moving
-    char UNKFlags[0x14];                                            // 0x2660
-
-    int ChangeWeaponHeldId;                                         // 0x2674
-
-    char UNK20[0x14];                                               // 0x2678
-
-    int WeaponHeldId;                                               // 0x268C
-
-    char UNK27[0x54];                                               // 0x2690
-
-    u32 WeaponHeldDataPointer;                                      // 0x26E4
-
-    char UNK21[0x738];                                              // 0x26E8
-    
-    float Health;                                                   // 0x2E20
-    
-    char UNK22[0xC4];                                               // 0x2E24
-
-    void * HeldMoby;                                                // 0x2EE8
-
-    void * UNK44;                                                   // 0x2EEC
-
-    // Points to the player moby
-    void * PlayerMoby;                                              // 0x2EF0
-
-    void * Vehicle;                                              // 0x2EF4
-
-    char UNK23[0x08];                                               // 0x2EF8
-
-    PadButtonStatus * Paddata;                                      // 0x2F00
-
-    char UNK24[0x10];                                               // 0x2F04
-
-    int Team;                                                       // 0x2F14
-
-    // Not super sure what this is
-    // But it seems to only get set when entering/leaving vehicles
-    // And setting it to non-zero stops the game from
-    // letting you get in another vehicle
-    int InVehicle;                                                  // 0x2F18
-
-    char UNK25[0x0C];                                               // 0x2F1C
-
-    void * RemotePadInfo;                                           // 0x2F28
-
-    char UNK36[0x84];                                               // 0x2F2C
-
-    float DamageMultiplier;                                         // 0x2FB0
-
-    char UNK26[0x0C];                                               // 0x2FB4
-
-    float Speed;                                                    // 0x2FC0
-
 } Player;
 
 /*
