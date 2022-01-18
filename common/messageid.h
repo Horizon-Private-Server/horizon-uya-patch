347538d2ba2aa2464a324bef891e763af48548da
Metroynome <yort124@gmail.com>
2022-01-18 08:42:40 -0500

    messageid.h: Added, no .c yet.

---
+++ common/messageid.h
@@ -0,0 +1,100 @@
+
+/***************************************************
+ * FILENAME :		messageid.h
+ * 
+ * DESCRIPTION :
+ * 		
+ * 		
+ * AUTHOR :			Daniel "Dnawrkshp" Gerendasy
+ */
+
+#ifndef _MESSAGEID_H_
+#define _MESSAGEID_H_
+
+/*
+ * NAME :		CustomMessageId
+ * 
+ * DESCRIPTION :
+ * 			Contains the different message ids.
+ * 
+ * NOTES :
+ * 
+ * 
+ * AUTHOR :			Daniel "Dnawrkshp" Gerendasy
+ */
+enum CustomMessageId
+{
+    UNUSED = 0,
+
+    /*
+     * Info on custom map.
+     */
+    CUSTOM_MSG_ID_SET_MAP_OVERRIDE = 1,
+
+    /*
+     * Sent to the server when the client wants the map loader irx modules.
+     */
+    CUSTOM_MSG_ID_CLIENT_REQUEST_MAP_IRX_MODULES = 2,
+
+    /*
+     * Sent after the server has sent all the map loader irx modules.
+     */
+    CUSTOM_MSG_ID_SERVER_SENT_MAP_IRX_MODULES = 3,
+
+    /*
+     * Sent in response to CUSTOM_MSG_ID_SET_MAP_OVERRIDE by the client.
+     */
+    CUSTOM_MSG_ID_SET_MAP_OVERRIDE_RESPONSE = 4,
+
+    /*
+     * Sent by the host when a game lobby has started.
+     */
+    CUSTOM_MSG_ID_GAME_LOBBY_STARTED = 5,
+
+    /*
+     *
+     */
+    CUSTOM_MSG_ID_SEARCH_AND_DESTROY_SET_OUTCOME = 6,
+
+    /*
+     * Sent from the client to the server when the client has updated their patch config.
+     */
+    CUSTOM_MSG_ID_CLIENT_USER_CONFIG = 7,
+
+    /*
+     * Sent from the client to the server when the client has destroyed a spleef box.
+     */
+    CUSTOM_MSG_ID_SPLEEF_DESTROY_BOX = 8,
+
+    /*
+     * Sent from the client to the server when the client wants to update the patch.
+     */
+    CUSTOM_MSG_ID_CLIENT_REQUEST_PATCH = 9,
+
+    /*
+     * Sent from the server to the host when the server wants the host to update all the teams
+     */
+    CUSTOM_MSG_ID_SERVER_REQUEST_TEAM_CHANGE = 10,
+    
+    /*
+     * Sent from the client to the server when the client has updated their patch game config.
+     */
+    CUSTOM_MSG_ID_CLIENT_USER_GAME_CONFIG = 11,
+
+    /*
+     * Sent from the server to the client when the host has changed and is propogating the patch game config.
+     */
+    CUSTOM_MSG_ID_SERVER_SET_GAME_CONFIG = 12,
+
+    /*
+     *
+     */
+    CUSTOM_MSG_ID_SEARCH_AND_DESTROY_SET_BOMB_OUTCOME = 13,
+
+    /*
+     *
+     */
+    CUSTOM_MSG_ID_CLIENT_SET_GAME_STATE = 14
+};
+
+#endif // _MESSAGEID_H_
