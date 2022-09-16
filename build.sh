a01edbc66764921e9e27b5874139f2d7a6d23708
John Janecek <janecektyler@gmail.com>
2022-09-16 00:02:37 -0600

    update

--- build.sh
+++ build.sh
@@ -1,2 +1,4 @@
-docker run -v $PWD\:/src ps2dev/ps2dev:v1.2.0 /bin/sh -c "cd src; sh container_commands.sh"
+docker run -v "${PWD}":/src ps2dev/ps2dev:v1.2.0 /bin/sh -c "cd src; sh container_commands.sh"
+
+cp bin/* ../horizon-server/docker/patch/
 
