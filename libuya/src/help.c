e9b15d35bcbef1ba749cb90e76c981553bd04fcd
Metroynome <yort124@gmail.com>
2023-01-13 21:11:36 -0500

    Updated

--- libuya/src/help.c
+++ libuya/src/help.c
@@ -1,21 +1,3 @@
 #include "help.h"
-#include "game.h"
 
-// 
-char * internal_helpGetString_inGame(int);
-char * internal_helpGetString_inLobby(int);
-
-//--------------------------------------------------------
-char * helpGetString(int stringId)
-{
-  if (isInGame())
-  {
-      return internal_helpGetString_inGame(stringId);
-  }
-  else if (isInMenus())
-  {
-      return internal_helpGetString_inLobby(stringId);
-  }
-
-  return NULL;
-}
+// Nothing to see here.  Function here was replaced with uiMsgString
