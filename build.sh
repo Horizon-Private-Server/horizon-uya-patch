f202816f064f4e749c270e7ef973b2c1f111117d
John Janecek <janecektyler@gmail.com>
2023-05-11 21:09:39 -0600

    Add irx modules, update build script (#7)

--- build.sh
+++ build.sh
@@ -1,6 +1,15 @@
+#!/bin/bash
+set -e
+
+sudo rm -rf bin
+mkdir bin
+
 docker run -v "${PWD}":/src ps2dev/ps2dev:v1.2.0 /bin/sh -c "cd src; sh container_commands.sh"
 
-cp bin/* ../horizon-server/docker/patch/
+cp bin/ ../horizon-server/docker/medius_plugins/bin -R
+cp bin/ ../horizon-server/docker/medius_plugins/bin/patch -R
 
-cp misc/* ../horizon-server/docker/patch/
+cp misc/* ../horizon-server/docker/medius_plugins/bin
+cp misc/* ../horizon-server/docker/medius_plugins/bin/patch
+
 
