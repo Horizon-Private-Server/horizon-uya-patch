d6df530e4fb74c2779f3004b569921f30fbcf7ef
Troy <yort124@gmail.com>
2023-05-28 16:58:47 -0400

    Fixed copying files into the wrong directory.
    Also added comments! :D

--- build.sh
+++ build.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 set -e
 
+# remove current and create new bin directory for horizon-uya-patch/
 sudo rm -rf bin
 mkdir bin
 
+# run docker and run "container_commands.sh"
 docker run -v "${PWD}":/src ps2dev/ps2dev:v1.2.0 /bin/sh -c "cd src; sh container_commands.sh"
 
-cp bin/ ../horizon-server/docker/medius_plugins/bin -R
-cp bin/ ../horizon-server/docker/medius_plugins/bin/patch -R
+# remove current and create new bin file in horizon-server/docker/medius_plugins/
+sudo rm -rf ../horizon-server/docker/medius_plugins/bin
+mkdir ../horizon-server/docker/medius_plugins/bin
 
+# copy misc/ binaries and patch files over to horizon-server.
 cp misc/* ../horizon-server/docker/medius_plugins/bin
-cp misc/* ../horizon-server/docker/medius_plugins/bin/patch
-
-
+cp bin/ ../horizon-server/docker/medius_plugins/bin/patch -R
