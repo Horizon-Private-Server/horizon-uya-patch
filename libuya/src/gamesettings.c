03b1479bb8d552b32e3ce3156366ebac8f96c185
Metroynome <yort124@gmail.com>
2023-06-12 20:38:32 -0400

    Added PAL addresses

--- libuya/src/gamesettings.c
+++ libuya/src/gamesettings.c
@@ -3,6 +3,29 @@
 #include "math.h"
 #include "help.h"
 
+#ifdef UYA_PAL
+
+/*
+ * Points to the active game settings.
+ */
+#define GLOBAL_GAMESETTINGS			            ((GameSettings*)(*(u32*)0x00241640))
+
+
+/*
+ * Points to the active game options.
+ */
+#define GLOBAL_GAMEOPTIONS			            ((GameOptions*)((u32)GLOBAL_GAMESETTINGS + 0x188))
+
+/*
+ *
+ */
+#define GAME_KILLS_TO_WIN                       (*(u8*)0x00241654)
+#define VEHICLES_ALLOWED                        (*(u8*)0x00241657)
+#define GAME_WEAPONS                            (*(u8*)0x0024165A)
+#define CTF_CAP_LIMIT                           (*(u8*)0x0024165C)
+
+#else
+
 /*
  * Points to the active game settings.
  */
@@ -21,6 +44,9 @@
 #define VEHICLES_ALLOWED                        (*(u8*)0x002417D7)
 #define GAME_WEAPONS                            (*(u8*)0x002417DA)
 #define CTF_CAP_LIMIT                           (*(u8*)0x002417DC)
+
+#endif
+
 
 
 //--------------------------------------------------------
