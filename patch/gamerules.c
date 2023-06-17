069cfd45703899659927c441031d7b9b99be1634
Metroynome <yort124@gmail.com>
2023-06-17 01:12:53 -0400

    Added gamerules.c

---
+++ patch/gamerules.c
@@ -0,0 +1,168 @@
+/***************************************************
+ * FILENAME :		gamerules.c
+ * 
+ * DESCRIPTION :
+ * 		Gamerules entrypoint and logic.
+ * 
+ * NOTES :
+ * 		
+ * AUTHOR :			Daniel "Dnawrkshp" Gerendasy
+ */
+
+#include <tamtypes.h>
+#include <string.h>
+
+#include <libuya/time.h>
+#include <libuya/game.h>
+#include <libuya/gamesettings.h>
+#include <libuya/map.h>
+#include <libuya/player.h>
+#include <libuya/stdio.h>
+#include <libuya/pad.h>
+#include <libuya/uya.h>
+#include <libuya/graphics.h>
+#include <libuya/math3d.h>
+#include <libuya/utils.h>
+#include <libuya/net.h>
+#include "module.h"
+#include "messageid.h"
+#include "include/config.h"
+#include "include/cheats.h"
+
+// config
+extern PatchConfig_t config;
+
+// game config
+extern PatchGameConfig_t gameConfig;
+
+// lobby clients patch config
+extern PatchConfig_t lobbyPlayerConfigs[GAME_MAX_PLAYERS];
+
+int GameRulesInitialized = 0;
+int FirstPass = 1;
+int HasDisabledHealthboxes = 0;
+short PlayerKills[GAME_MAX_PLAYERS];
+
+
+/*
+ * NAME :		grInitialize
+ * 
+ * DESCRIPTION :
+ * 			Initializes this module.
+ * 
+ * NOTES :
+ * 			This is called only when in game.
+ * 
+ * ARGS : 
+ * 
+ * RETURN :
+ * 
+ * AUTHOR :			Daniel "Dnawrkshp" Gerendasy
+ */
+void grInitialize(void)
+{
+	int i;
+
+	// Initialize player kills to 0
+	for (i = 0; i < GAME_MAX_PLAYERS; ++i)
+		PlayerKills[i] = 0;
+
+	HasDisabledHealthboxes = 0;
+	GameRulesInitialized = 1;
+}
+
+/*
+ * NAME :		grGameStart
+ * 
+ * DESCRIPTION :
+ * 			Gamerules game logic entrypoint.
+ * 
+ * NOTES :
+ * 			This is called only when in game.
+ * 
+ * ARGS : 
+ * 
+ * RETURN :
+ * 
+ * AUTHOR :			Daniel "Dnawrkshp" Gerendasy
+ */
+void grGameStart(void)
+{
+	int i = 0;
+	GameSettings * gameSettings = gameGetSettings();
+
+	// Initialize
+	if (GameRulesInitialized != 1)
+		grInitialize();
+
+	if (gameConfig.disableWeaponPacks == 1)
+		disableWeaponPacks();
+	else if (gameConfig.disableWeaponPacks == 2)
+		spawnWeaponPackOnDeath();
+	
+	if (gameConfig.disableV2s)
+		disableV2s();
+
+    if (gameConfig.disableHealthBoxes && !HasDisabledHealthboxes)
+		HasDisabledHealthboxes = disableHealthboxes();
+
+	FirstPass = 0;
+}
+
+/*
+ * NAME :		grLobbyStart
+ * 
+ * DESCRIPTION :
+ * 			Gamerules lobby logic entrypoint.
+ * 
+ * NOTES :
+ * 			This is called only when in lobby.
+ * 
+ * ARGS : 
+ * 
+ * RETURN :
+ * 
+ * AUTHOR :			Daniel "Dnawrkshp" Gerendasy
+ */
+void grLobbyStart(void)
+{
+	GameSettings * gameSettings = gameGetSettings();
+
+	// If we're not in staging then reset
+	if (gameSettings)
+		return;
+
+	// Reset
+	GameRulesInitialized = 0;
+	FirstPass = 1;
+}
+
+
+/*
+ * NAME :		grLoadStart
+ * 
+ * DESCRIPTION :
+ * 			Gamerules load logic entrypoint.
+ * 
+ * NOTES :
+ * 			This is called only when the game has finished reading the level from the disc and before it has started processing the data.
+ * 
+ * ARGS : 
+ * 
+ * RETURN :
+ * 
+ * AUTHOR :			Daniel "Dnawrkshp" Gerendasy
+ */
+void grLoadStart(void)
+{
+	// only handle when loading level
+	GameSettings* gs = gameGetSettings();
+	if (!gs || gs->GameStartTime >= 0)
+		return;
+
+  //gameGetOptions()->GameFlags.MultiplayerGameFlags.Timelimit = 1;
+
+	// Hook load gameplay file
+	// if (*(u32*)0x004EE598 == 0x0C13B1C8)
+	// 	*(u32*)0x004EE598 = 0x0C000000 | (u32)&onGameplayLoad / 4;
+}
