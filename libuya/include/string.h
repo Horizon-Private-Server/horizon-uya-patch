617469aa00dd51e42dda3a7516e134a760bc24e9
Metroynome <yort124@gmail.com>
2023-07-29 21:20:40 -0400

    added more string functions.

--- libuya/include/string.h
+++ libuya/include/string.h
@@ -16,5 +16,9 @@
 __LIBUYA_INLINE__ void * memset(void * ptr, int value, int num);
 __LIBUYA_INLINE__ void * memcpy(void * destination, const void * source, int num);
 __LIBUYA_INLINE__ char * strncpy(char *dest, const char *src, int n);
+__LIBUYA_INLINE__ char * strcat(char *dest, const char *src);
+__LIBUYA_INLINE__ char * strncat(char *dest, const char *src, int n);
+__LIBUYA_INLINE__ int strcmp(char *str1, char *str2);
+__LIBUYA_INLINE__ int strncmp(char * str1, char * str2, int n);
 
 #endif // _LIBUYA_STRING_H_
