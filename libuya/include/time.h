39be9a6c630c565568c463a791f7f26e4f1d0da3
Metroynome <yort124@gmail.com>
2023-08-22 16:52:51 -0400

    added Systemclock function

--- libuya/include/time.h
+++ libuya/include/time.h
@@ -15,5 +15,22 @@
 
 #define TIME_SECOND             (1000)
 #define TIME_MINUTE             (TIME_SECOND * 60)
+#define SYSTEM_TIME_TICKS_PER_MS  (0x24000)
+
+/*
+ * NAME :		timerGetSystemTime
+ * 
+ * DESCRIPTION :
+ * 			Returns system time.
+ * 
+ * NOTES :
+ * 
+ * ARGS : 
+ * 
+ * RETURN :
+ * 
+ * AUTHOR :			Daniel "Dnawrkshp" Gerendasy
+ */
+long timerGetSystemTime(void);
 
 #endif // _LIBUYA_TIME_H_
