158ad47d2984ad3f46dda9010cb8da3c87276c63
Metroynome <yort124@gmail.com>
2023-09-30 22:04:09 -0400

    Added matrix functions

--- libuya/include/math3d.h
+++ libuya/include/math3d.h
@@ -27,9 +27,15 @@ typedef float VECTOR[4] __attribute__((__aligned__(16)));
 typedef float MATRIX[16] __attribute__((__aligned__(16)));
 
 //--------------------------------------------------------
+void vector_write(VECTOR output, u128 input0);
+u128 vector_read(VECTOR input0);
+float vector_sqrmag(VECTOR input0);
+void vector_reflect(VECTOR output, VECTOR input0, VECTOR normal);
+void vector_fromyaw(VECTOR out, float radians);
 void vector_apply(VECTOR output, VECTOR input0, MATRIX input1);
 void vector_copy(VECTOR output, VECTOR input0);
 void vector_normalize(VECTOR output, VECTOR input0);
+float vector_innerproduct_unscaled(VECTOR input0, VECTOR input1);
 float vector_innerproduct(VECTOR input0, VECTOR input1);
 void vector_outerproduct(VECTOR output, VECTOR input0, VECTOR input1);
 float vector_length(VECTOR input0);
@@ -38,22 +44,19 @@ void vector_subtract(VECTOR output, VECTOR input0, VECTOR input1);
 void vector_add(VECTOR output, VECTOR input0, VECTOR input1);
 void vector_negate(VECTOR output, VECTOR input0);
 void vector_scale(VECTOR output, VECTOR input0, float scalar);
+void vector_multiply(VECTOR output, VECTOR input0, VECTOR input1);
+void vector_projectonvertical(VECTOR output, VECTOR input0);
+void vector_projectonhorizontal(VECTOR output, VECTOR input0);
 void vector_print(VECTOR input0);
 
-void quaternion_fromeuler(VECTOR output, VECTOR input0);
-void quaternion_toeuler(VECTOR output, VECTOR input0);
-void quaternion_lookrotation(VECTOR output, VECTOR input0, VECTOR input1);
-void quaternion_multiply(VECTOR output, VECTOR input0, VECTOR input1);
-void quaternion_multiplyvector(VECTOR output, VECTOR q, VECTOR v);
-void quaternion_frommatrix(VECTOR output, MATRIX input0);
-
-void matrix_fromquaternion(MATRIX output, VECTOR input0);
 void matrix_toeuler(VECTOR output, MATRIX input0);
 void matrix_fromrows(MATRIX output, VECTOR input0, VECTOR input1, VECTOR input2, VECTOR input3);
 void matrix_copy(MATRIX output, MATRIX input0);
 void matrix_inverse(MATRIX output, MATRIX input0);
 void matrix_multiply(MATRIX output, MATRIX input0, MATRIX input1);
-void matrix_rotate(MATRIX output, MATRIX input0, VECTOR input1);
+void matrix_rotate_x(MATRIX output, MATRIX input0, float radians);
+void matrix_rotate_y(MATRIX output, MATRIX input0, float radians);
+void matrix_rotate_z(MATRIX output, MATRIX input0, float radians);
 void matrix_scale(MATRIX output, MATRIX input0, VECTOR input1);
 void matrix_translate(MATRIX output, MATRIX input0, VECTOR input1);
 void matrix_transpose(MATRIX output, MATRIX input0);
