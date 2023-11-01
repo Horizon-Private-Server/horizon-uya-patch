d08b641fe7e882fb758f76d3a490b48e00f587ca
Metroynome <yort124@gmail.com>
2023-11-01 13:43:18 -0400

    Updated Net

--- libuya/include/net.h
+++ libuya/include/net.h
@@ -17,6 +17,13 @@
 #define NET_CUSTOM_MESSAGE_ID                       (8)
 #define NET_LOBBY_CLIENT_INDEX                      (0xFFFF)
 
+// ensures packets arrive in order
+#define NET_ORDER_CRITICAL                          (0x10)
+// sends reliably
+#define NET_DELIVERY_CRITICAL                       (0x40)
+// skips aggregation
+#define NET_LATENCY_CRITICAL                        (0x80)
+
 typedef int (*NET_CALLBACK_DELEGATE)(void * connection, void * data);
 
 void netInstallCustomMsgHook(int force);
