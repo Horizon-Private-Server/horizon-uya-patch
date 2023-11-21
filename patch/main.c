8ad6a188cd986390cb6e56fb7db5087dd092023b
Metroynome <yort124@gmail.com>
2023-11-20 22:01:15 -0500

    TEST: CTF Flag Custom Logic

--- patch/main.c
+++ patch/main.c
@@ -29,6 +29,7 @@
 #include <libuya/utils.h>
 #include <libuya/player.h>
 #include <libuya/map.h>
+#include <libuya/guber.h>
 #include "module.h"
 #include "messageid.h"
 #include "config.h"
@@ -101,6 +102,7 @@ extern void* _correctTieLod;
 extern int _correctTieLod_Jump;
 extern VariableAddress_t vaPlayerRespawnFunc;
 extern VariableAddress_t vaPlayerSetPosRotFunc;
+extern VariableAddress_t vaFlagUpdate_Func;
 extern MenuElem_ListData_t dataCustomMaps;
 extern SelectedCustomMapId;
 
@@ -1483,6 +1485,288 @@ void patchMapAndScoreboardToggle(void)
 	}
 }
 
+void flagHandlePickup(Moby* flagMoby, int pIdx)
+{
+	Player** players = playerGetAll();
+	Player* player = players[pIdx];
+	if (!player || !flagMoby)
+		return;
+	
+	struct FlagPVars* pvars = (struct FlagPVars*)flagMoby->PVar;
+	if (!pvars)
+		return;
+
+	// fi flag state isn't 1
+	if (flagMoby->State != 1)
+		return;
+
+	// flag is currently returning
+	if (flagIsReturning(flagMoby))
+		return;
+
+	// flag is currently being picked up
+	if (flagIsBeingPickedUp(flagMoby))
+		return;
+
+	// only allow actions by living players
+	if (playerIsDead(player) || playerGetHealth(player) <= 0)
+		return;
+
+	// Handle pickup/return
+	if (player->mpTeam == pvars->Team) {
+		flagReturnToBase(flagMoby, 0, pIdx);
+	} else {
+		flagPickup(flagMoby, pIdx);
+		player->FlagMoby = flagMoby;
+	}
+	DPRINTF("player %d picked up flag %X at %d\n", player->PlayerId, flagMoby->OClass, gameGetTime());
+}
+
+/*
+ * NAME :		flagRequestPickup
+ * 
+ * DESCRIPTION :
+ * 			Requests to either pickup or return the given flag.
+ * 			If host, this request is automatically passed to the handler.
+ * 			If not host, this request is sent over the net to the host.
+ * 
+ * NOTES :
+ * 
+ * ARGS : 
+ * 
+ * RETURN :
+ * 
+ * AUTHOR :			Daniel "Dnawrkshp" Gerendasy
+ */
+void flagRequestPickup(Moby* flagMoby, int pIdx)
+{
+	static int requestCounters[GAME_MAX_PLAYERS] = {0,0,0,0,0,0,0,0,0,0};
+	Player** players = playerGetAll();
+	Player* player = players[pIdx];
+	if (!player || !flagMoby)
+		return;
+	struct FlagPVars* pvars = (struct FlagPVars*)flagMoby->PVar;
+	if (!pvars)
+		return;
+
+	if (gameAmIHost()) {
+		// handle locally
+		flagHandlePickup(flagMoby, pIdx);
+	} else if (requestCounters[pIdx] == 0) {
+		// send request to host
+		void* dmeConnection = netGetDmeServerConnection();
+		if (dmeConnection) {
+			ClientRequestPickUpFlag_t msg;
+			msg.GameTime = gameGetTime();
+			msg.PlayerId = player->mpIndex;
+			msg.FlagUID = guberGetUID(flagMoby);
+			netSendCustomAppMessage(dmeConnection, gameGetHostId(), CUSTOM_MSG_ID_FLAG_REQUEST_PICKUP, sizeof(ClientRequestPickUpFlag_t), &msg);
+			requestCounters[pIdx] = 10;
+			DPRINTF("sent request flag pickup %d\n", gameGetTime());
+		}
+	}
+	else
+	{
+		requestCounters[pIdx]--;
+	}
+}
+
+/*
+ * NAME :		customFlagLogic
+ * 
+ * DESCRIPTION :
+ * 			Reimplements flag pickup logic but runs through our host authoritative logic.
+ * 
+ * NOTES :
+ * 
+ * ARGS : 
+ * 
+ * RETURN :
+ * 
+ * AUTHOR :			Daniel "Dnawrkshp" Gerendasy
+ */
+void customFlagLogic(Moby* flagMoby)
+{
+	VECTOR t;
+	int i;
+	Player** players = playerGetAll();
+	int gameTime = gameGetTime();
+	GameOptions* gameOptions = gameGetOptions();
+
+	// if not in game
+	if (!isInGame())
+		return;
+
+	// if flagMoby doesn't exist
+	if (!flagMoby)
+		return;
+
+	// if flag pvars don't exist
+	struct FlagPVars* pvars = (struct FlagPVars*)flagMoby->PVar;
+	if (!pvars)
+		return;
+
+	// if flag state doesn't equal 1
+	if (flagMoby->State != 1)
+		return;
+
+	// if flag is returning
+	if (flagIsReturning(flagMoby))
+		return;
+
+	// if flag is being picked up
+	if (flagIsBeingPickedUp(flagMoby))
+		return;
+
+	// return to base if flag has been idle for 40 seconds
+	if ((pvars->TimeFlagDropped + (TIME_SECOND * 40)) < gameTime && !flagIsAtBase(flagMoby)) {
+		flagReturnToBase(flagMoby, 0, 0xFF);
+		return;
+	}
+
+	// if flag didn't land on safe ground, return flag.
+	// if (!flagIsOnSafeGround(flagMoby)) {
+	// 	flagReturnToBase(flagMoby, 0, 0xFF);
+	// 	return;
+	// }
+
+	for (i = 0; i < GAME_MAX_PLAYERS; ++i) {
+		Player* player = players[i];
+		if (!player || !player->IsLocal)
+			continue;
+
+		// wait 3 seconds for last carrier to be able to pick up again
+		if ((pvars->TimeFlagDropped + 3*TIME_SECOND) > gameTime && i == pvars->LastCarrierIdx)
+			continue;
+
+		// only allow actions by living players
+		if (playerIsDead(player) || playerGetHealth(player) <= 0)
+			continue;
+
+		// skip player if they've only been alive for < 3 seconds
+		if (player->timers.timeAlive <= 180)
+			continue;
+
+		// skip player if in vehicle
+		if (player->Vehicle)
+			continue;
+
+		// skip if player state is in vehicle and critterMode is on
+		if (player->Camera && player->State == PLAYER_STATE_VEHICLE)
+			continue;
+
+		// skip if player is on teleport pad
+		if (player->ground.pMoby && player->ground.pMoby->OClass == MOBY_ID_TELEPORT_PAD)
+			continue;
+
+		// player must be within 2 units of flag
+		vector_subtract(t, flagMoby->Position, player->PlayerPosition);
+		float sqrDistance = vector_sqrmag(t);
+		if (sqrDistance > (2*2))
+			continue;
+
+		// player is on different team than flag and player isn't already holding flag
+		if (player->mpTeam != pvars->Team) {
+			if (!player->FlagMoby) {
+				flagRequestPickup(flagMoby, i);
+				return;
+			}
+		} else {
+			// if player is on same team as flag and close enough to return it
+			vector_subtract(t, pvars->BasePosition, flagMoby->Position);
+			float sqrDistanceToBase = vector_sqrmag(t);
+			if (sqrDistanceToBase > 0.1) {
+				flagRequestPickup(flagMoby, i);
+				return;
+			}
+		}
+	}
+}
+
+/*
+ * NAME :		onRemoteClientRequestPickUpFlag
+ * 
+ * DESCRIPTION :
+ * 			Handles when a remote client sends the CUSTOM_MSG_ID_FLAG_REQUEST_PICKUP message.
+ * 
+ * NOTES :
+ * 
+ * ARGS : 
+ * 
+ * RETURN :
+ * 
+ * AUTHOR :			Daniel "Dnawrkshp" Gerendasy
+ */
+int onRemoteClientRequestPickUpFlag(void * connection, void * data)
+{
+	int i;
+	ClientRequestPickUpFlag_t msg;
+	Player** players;
+	memcpy(&msg, data, sizeof(msg));
+
+	// ignore if not in game
+	if (!isInGame() || !gameAmIHost())
+		return sizeof(ClientRequestPickUpFlag_t);
+
+	DPRINTF("remote player %d requested pick up flag %X at %d\n", msg.PlayerId, msg.FlagUID, msg.GameTime);
+
+	// get list of players
+	players = playerGetAll();
+
+	// get remote player or ignore message
+	Player** allRemote = playerGetAll();
+	Player* remotePlayer = allRemote[msg.PlayerId];
+	if (!remotePlayer)
+		return sizeof(ClientRequestPickUpFlag_t);
+
+	// get flag
+	GuberMoby* gm = (GuberMoby*)guberGetObjectByUID(msg.FlagUID);
+	if (gm && gm->Moby) {
+		Moby* flagMoby = gm->Moby;
+		flagHandlePickup(flagMoby, msg.PlayerId);
+	}
+
+	return sizeof(ClientRequestPickUpFlag_t);
+}
+
+void patchCTFFlag(void)
+{
+	VECTOR t;
+	int i = 0;
+	Player** players = playerGetAll();
+
+	if (!isInGame())
+		return;
+
+	netInstallCustomMsgHandler(CUSTOM_MSG_ID_FLAG_REQUEST_PICKUP, &onRemoteClientRequestPickUpFlag);
+
+	u32 FlagFunc = GetAddress(&vaFlagUpdate_Func);
+	if (FlagFunc){
+		*(u32*)FlagFunc = 0x03e00008;
+		*(u32*)(FlagFunc + 0x4) = 0x0000102D;
+	}
+
+	GuberMoby* gm = guberMobyGetFirst();
+	while (gm) {
+		if (gm->Moby) {
+			switch (gm->Moby->OClass) {
+				case MOBY_ID_CTF_RED_FLAG:
+				case MOBY_ID_CTF_BLUE_FLAG:
+				{
+					customFlagLogic(gm->Moby);
+					// gm->Update = &customFlagLogic;
+					// Master * master = masterGet(gm->Guber.Id.UID);
+					// master->Update = 0;
+
+					break;
+				}
+			}
+		}
+		gm = (GuberMoby*)gm->Guber.Next;
+	}
+	return;
+}
+
 /*
  * NAME :		runGameStartMessager
  * 
@@ -1855,6 +2139,9 @@ int main(void)
 
 		// Patch Death Barrier Bug/Teleporter Glitch
 		patchDeathBarrierBug();
+
+		// Patch CTF Flag Logic with our own.
+		// patchCTFFlag();
 
 		// Patch Flux Niking
 		if (gameConfig.grFluxNikingDisabled)
