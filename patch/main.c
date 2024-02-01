6abcbe59cd7ec50cb28ed4435ca52eb31dc24a9b
John Janecek <yort124@gmail.com>
2024-02-01 12:08:27 -0600

    updated onServerDate and setupPatchConfigInGame

--- patch/main.c
+++ patch/main.c
@@ -292,7 +292,8 @@ void onServerTimeResponse(void* connection, void* data)
 	memcpy(&response, data, sizeof(DateResponse_t));
 	DPRINTF("\nDate: %d/%d", response.Month, response.Day);
 	patchPointers.ServerTimeMonth = response.Month;
-	patchPointers.ServerTimeDay = response.Day;
+	// not adding 1 to the date broke things on January 31st.
+	patchPointers.ServerTimeDay = response.Day + 1;
 }
 
 //------------------------------------------------------------------------------
@@ -2507,10 +2508,12 @@ void setupPatchConfigInGame(void)
 	// u32 ConfigEnableFunc = 0x0C000000 | ((u32)&configMenuEnable >> 2);
 	// Original If: *(u32*)(Addr + 0x8) != ConfigEnableFunc
 	if (!patched) {
+		static u32 Addr = 0;
 		// Get Menu address via current map.
-   		static u32 Addr = 0;
-		if (!Addr)
+		if (!Addr) {
 			Addr = GetAddress(&vaPauseMenuAddr);
+			return;
+		}
 
 		// Insert needed ID, returns string.
 		int str = uiMsgString(0x1115); // Washington, D.C. string ID
