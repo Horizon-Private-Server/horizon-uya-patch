14e3288a9a82953e7f6f0c08da568c9814e3f4cb
Metroynome <yort124@gmail.com>
2024-03-30 23:11:32 -0400

    Updated Number of Locals

--- libuya/include/game.h
+++ libuya/include/game.h
@@ -17,7 +17,7 @@
  * Maximum number of players in a game.
  */
 #define GAME_MAX_PLAYERS                    (8)
-#define GAME_MAX_LOCALS                     (2)
+#define GAME_MAX_LOCALS                     (1)
 /*
  * Games FPS
 */
