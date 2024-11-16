5cf6d7b94ffbec5656323e28c949cb1c0fae5b4d
Metroynome <yort124@gmail.com>
2024-11-16 14:20:50 -0500

    Updated UpdateGameStateRequest struct.

--- common/module.h
+++ common/module.h
@@ -97,10 +97,10 @@ typedef struct UpdateGameStateRequest {
 	char TeamsEnabled;
     char PADDING;
     short Version;
-    int Nodes[2]; // Blue Team, Red Team
 	int TeamScores[GAME_MAX_PLAYERS];
 	char ClientIds[GAME_MAX_PLAYERS];
 	char Teams[GAME_MAX_PLAYERS];
+    int Nodes[2]; // Blue Team, Red Team
 } UpdateGameStateRequest_t;
 
 typedef struct CustomGameModeStats
