580da290cb8c10c5b696915a01e7b004ccb860de
Metroynome <yort124@gmail.com>
2025-03-07 18:04:52 -0500

    revert

--- libuya/include/common.h
+++ libuya/include/common.h
@@ -11,11 +11,4 @@
 #define __LIBUYA_SETTER__    inline
 #define __LIBUYA_INLINE__    inline
 
-typedef unsigned char u8;
-typedef unsigned short u16;
-typedef unsigned int u32;
-typedef long unsigned int u64;
-typedef long long unsigned int u128;
-
-
 #endif // _LIBUYA_COMMON_H_
