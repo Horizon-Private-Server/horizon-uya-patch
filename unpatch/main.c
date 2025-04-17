71960cac39a75274e9c6b9e7b7d52fb50ff13fa1
Metroynome <yort124@gmail.com>
2025-04-17 11:21:13 -0500

    Updated CGM location.  No longer overwrites important data

--- unpatch/main.c
+++ unpatch/main.c
@@ -76,8 +76,8 @@ const int patches[][3] = {
 
 const int clears[][2] = {
 	{ 0x000d0000, 0x00010000 }, // usb modules
-	{ 0x000e0000, 0x00015000 }, // patch
-	{ 0x000f5000, 0x0000a000 }, // game mode
+	{ 0x000e0000, 0x0001a000 }, // patch
+	{ 0x000fa000, 0x00006000 }, // game mode
 	{ 0x000cf000, 0x00000800 }, // module definitions
 	{ 0x000cffd0, 0x00000020 }, // patch hash
 	{ 0x000cffc0, 0x00000010 }, // patch pointers
