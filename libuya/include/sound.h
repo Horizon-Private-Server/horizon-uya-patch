b3cfb19cc90e85b7b5c6d98149943bfcb36746ae
Metroynome <yort124@gmail.com>
2025-11-22 10:05:21 -0500

    Moved SOUND_GLOBAL pointer to sound.h

--- libuya/include/sound.h
+++ libuya/include/sound.h
@@ -13,6 +13,14 @@
 #include "player.h"
 #include "spawnpoint.h"
 
+#if UYA_PAL
+#define SOUND_GLOBALS ((SoundGlobals*)0x001fe020)
+#define AUDIO_INFO_DATA ((stAudioInfoData*)0)
+#else
+#define SOUND_GLOBALS ((SoundGlobals*)0x001fe1a0)
+#define AUDIO_INFO_DATA ((stAudioInfoData*)0x00240700)
+#endif
+
 // All sounds were played in Bank 0
 typedef enum SOUND_IDS {
 	SOUND_ID_MENU_HELP_MESSAGE = 0,
@@ -56,7 +64,7 @@ struct SoundInstance { // 0x90
 	/* 0x10 */ struct Cuboid cuboid;
 };
 
-struct _sound_data { // 0x80
+typedef struct _sound_data { // 0x80
 	/* 0x00 */ int handle;
 	/* 0x04 */ struct SoundDef *def;
 	/* 0x08 */ short int index;
@@ -81,38 +89,28 @@ struct _sound_data { // 0x80
 	/* 0x58 */ u16 unique_id;
 	/* 0x5a */ u16 pad16_0;
 	/* 0x5c */ char occl[36];
-};
+} SoundData;
 
 typedef struct _sound_globals { // 0x1d30
-	/* 0x0000 */ int sfxVol;
-	/* 0x0004 */ int musicVol;
-	/* 0x0008 */ int dialogVol;
-	/* 0x000c */ int ambientVol;
-	/* 0x0010 */ int shellVol;
-	/* 0x0014 */ int mpegVol;
-	/* 0x0018 */ int pad32_0[2];
-	/* 0x0020 */ int vol_backup[6];
-	/* 0x0038 */ int pad32_1[2];
-	/* 0x0040 */ char reverb_type;
-	/* 0x0041 */ char reverb_delay;
-	/* 0x0042 */ char reverb_feedback;
-	/* 0x0043 */ char reverb_changed;
-	/* 0x0044 */ struct Moby *occl_ignore;
-	/* 0x0048 */ int cam_history_pos;
-	/* 0x004c */ int load_busy;
-	/* 0x0050 */ VECTOR cam_history[4];
-	/* 0x0090 */ void *handles[132];
-	/* 0x02a0 */ int cam_ofs_frame;
-	/* 0x02a4 */ short int kludgeVolState;
-	/* 0x02a6 */ short int kludgeVolTimer;
-	/* 0x02a8 */ struct SoundInstance *pReverbOwner;
-	/* 0x02ac */ int reverb_depth;
-	/* 0x02b0 */ int ambientCnt;
-	/* 0x02b4 */ struct SoundInstance *pAmbient;
-	/* 0x02c0 */ struct _sound_data channels[52];
-	/* 0x1cc0 */ VECTOR cam_ofs[6];
-	/* 0x1d20 */ u32 volFlags;
-	/* 0x1d24 */ int pad32_2[3];
+/* 0x0000 */ int sfxVol;
+/* 0x0004 */ int musicVol;
+/* 0x0008 */ int dialogVol;
+/* 0x000c */ int ambientVol;
+/* 0x0010 */ int shellVol;
+/* 0x0014 */ int mpegVol;
+/* 0x0018 */ int pad32_0[2];
+/* 0x0020 */ int vol_backup[6];
+/* 0x0038 */ int pad32_1[2];
+/* 0x0040 */ char reverb_type;
+/* 0x0041 */ char reverb_delay;
+/* 0x0042 */ char reverb_feedback;
+/* 0x0043 */ char reverb_changed;
+/* 0x0044 */ struct Moby *occl_ignore;
+/* 0x0048 */ int cam_history_pos;
+/* 0x004c */ int load_busy;
+/* 0x0050 */ VECTOR cam_history[4];
+/* 0x0090 */ void *functions[10];
+/* 0x00b8 */ SoundData channels[52];
 } SoundGlobals;
 
 typedef struct SoundDef { // 0x20
@@ -150,8 +148,5 @@ void soundHeroPlay(Player* player, short sound, int flags);
 void soundMobyPlay(short sound, int flags, Moby* moby);
 void soundPlayByOClass(short sound, int flags, Moby* moby, short oClass);
 void soundPlay(SoundDef* sound, int flags, Moby* moby, VECTOR position, int volume);
-
-// Return Sound Globals
-// SoundGlobals* soundGetSound(void);
 
 #endif // _LIBUYA_SOUND_H_
