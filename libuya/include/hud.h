c2a20230eb4abfe438e3ee93c5643fac528d6dad
Metroynome <yort124@gmail.com>
2025-12-27 01:25:04 -0500

    Many Widget structs added.

--- libuya/include/hud.h
+++ libuya/include/hud.h
@@ -3,12 +3,22 @@
 
 #include <tamtypes.h>
 #include "common.h"
+#include "math3d.h"
+
+typedef unsigned int HANDLE_ID;
 
 typedef enum eEngineState {
     ENGINE_IDLE = 0,
     ENGINE_READY = 1,
     ENGINE_PAUSED = 2
 } EngineState_e;
+
+typedef enum eTextAlignment {
+	TEXT_ALIGN_LEFT = 0,
+	TEXT_ALIGN_CENTER = 1,
+	TEXT_ALIGN_RIGHT = 2,
+	TEXT_ALIGN_MAX = 3
+} TextAlignment_e;
 
 struct MapOffsets { // 0x20
 /* 0x00 */ float min_x;
@@ -104,6 +114,225 @@ typedef struct ConcretePreLoadedImageBuffer {
 /* 0x0c */ void *memory_buffer;
 /* 0x10 */ void *decompressed_buffer;
 } ConcretePreLoadedImageBuffer_t;
+
+struct HudWidgetTex { // 0x10
+/* 0x0 */ u64 tex;
+/* 0x8 */ short unsigned int tex_ofs;
+/* 0xa */ short unsigned int pal_ofs;
+/* 0xc */ short int u_log;
+/* 0xe */ short int v_log;
+};
+
+struct HudWidget { // 0x30
+/* 0x00 */ short int vert_cnt;
+/* 0x02 */ short int poly_cnt;
+/* 0x04 */ short int joint_cnt;
+/* 0x06 */ short int tex_cnt;
+/* 0x08 */ short int frame_cnt;
+/* 0x0a */ short int frame_size;
+/* 0x0c */ float scale;
+/* 0x10 */ short int *verts;
+/* 0x14 */ char *polys;
+/* 0x18 */ struct HudWidgetTex *tex_defs;
+/* 0x1c */ void *tex_data;
+/* 0x20 */ void *frames;
+/* 0x24 */ int vert_format;
+/* 0x28 */ int rgba;
+/* 0x2c */ int pad;
+};
+
+struct Event { // 0xc
+/* 0x0 */ HANDLE_ID targetWidget;
+/* 0x4 */ unsigned int eventType;
+/* 0x8 */ unsigned int data;
+};
+
+struct EventArray { // 0xc0
+/* 0x00 */ struct Event m_events[16];
+};
+
+struct EventResponseTOC { // 0xc0
+/* 0x00 */ struct EventArray *m_array[48];
+};
+
+struct fontLetter { // 0x4
+/* 0x0 */ unsigned char u;
+/* 0x1 */ unsigned char v;
+/* 0x2 */ signed char drop;
+/* 0x3 */ signed char spacing;
+};
+
+struct FreeNode { // 0x4
+/* 0x0 */ struct FreeNode *mpNext;
+};
+
+struct ConstantBlockSizeMemoryManager { // 0x18
+/* 0x00 */ unsigned char *staticBlock;
+/* 0x04 */ unsigned int heapSize;
+/* 0x08 */ unsigned int allocSize;
+/* 0x0c */ unsigned int used;
+/* 0x10 */ unsigned int currentlyAllocated;
+/* 0x14 */ struct FreeNode *freeList;
+};
+
+struct iObject {
+/* 0x0 */ u32 refCount;
+/* 0x4 */ u8 control_flags;
+/* 0x8 */ int *vtable;
+};
+
+struct iGraphicsObject { // 0x28
+/* 0x00 */ vec4f *positionChannel;
+/* 0x04 */ vec4f *scaleChannel;
+/* 0x08 */ vec4f *rotationChannel;
+/* 0x0c */ vec4f *globalColorChannel;
+/* 0x10 */ vec4f *visibilityChannel;
+/* 0x14 */ bool bExternalPositionChannel;
+/* 0x15 */ bool bExternalScaleChannel;
+/* 0x16 */ bool bExternalRotationChannel;
+/* 0x17 */ bool bExternalGlobalColorChannel;
+/* 0x18 */ bool bExternalVisibilityChannel;
+/* 0x1c */ char *myName;
+/* 0x20 */ struct ConstantBlockSizeMemoryManager *channelPoolManager;
+/* 0x24 */ int *vtable;
+};
+
+struct iFrame {
+/* 0x00 */ struct iObject object;
+/* 0x0c */ u32 controlFlags;
+/* 0x10 */ float rot;
+/* 0x14 */ vec2f pos;
+/* 0x1c */ vec2f scale;
+/* 0x24 */ vec2f dropshadow_offset;
+/* 0x2c */ float alpha;
+/* 0x30 */ u32 color;
+/* 0x34 */ u32 dropshadow_color;
+/* 0x38 */ int animation_id;
+/* 0x3c */ struct EventResponseTOC *m_event_listen_toc;
+};
+
+typedef struct WidgetRectangle {
+/* 0x00 */ struct iFrame frame;
+/* 0x40 */ float rotation;
+/* 0x44 */ u32 color1;
+/* 0x48 */ u32 color2;
+/* 0x4c */ u32 color3;
+/* 0x50 */ u16 textureId;
+/* 0x52 */ u16 frameNum;
+/* 0x54 */ char total_fade_frames;
+/* 0x55 */ char fade_frame;
+/* 0x56 */ char renderstate;
+/* 0x58 */ struct DataSource *datasource;
+} WidgetRectangle_t;
+
+typedef struct WidgetHallowRectangle { // 0x48
+/* 0x00 */ struct iFrame frame;
+/* 0x40 */ vec2f insets;
+} WidgetHallowRectangle_t;
+
+typedef struct WidgetText { // 0x70
+/* 0x00 */ struct iFrame frame;
+/* 0x40 */ vec2f rot;
+/* 0x48 */ int fontTable;
+/* 0x4c */ char *pExternalStringMemory;
+/* 0x50 */ unsigned int font_FX;
+/* 0x54 */ unsigned char widget_text_control;
+/* 0x58 */ float maxWidth;
+/* 0x5c */ float minimumScale;
+/* 0x60 */ unsigned char pointSize;
+/* 0x64 */ int fontText;
+/* 0x68 */ vec2f cached_scale;
+} WidgetText_t;
+
+typedef struct WidgetTextAreaData { // 0x54
+/* 0x00 */ vec2f rot;
+/* 0x08 */ int FontTable;
+/* 0x0c */ char *pExternalStringMemory;
+/* 0x10 */ char **pExternalStringListHandle;
+/* 0x14 */ short int stringListCnt;
+/* 0x16 */ short int selectedIdx;
+/* 0x18 */ unsigned int font_FX;
+/* 0x1c */ bool bAutoScale;
+/* 0x1e */ short unsigned int max_width;
+/* 0x20 */ float min_scale;
+/* 0x24 */ float text_scale;
+/* 0x28 */ vec2f selectedLine;
+/* 0x30 */ u32 colorSelected;
+/* 0x34 */ vec2f Scroll;
+/* 0x3c */ vec2f line_spacing;
+/* 0x40 */ short unsigned int widget_text_area_control;
+/* 0x42 */ unsigned char pointSize;
+/* 0x44 */ short int scroll_delay;
+/* 0x48 */ float scroll_speed;
+/* 0x4c */ short int scroll_repeat;
+/* 0x4e */ short int scroll_delay_counter;
+/* 0x50 */ char font_text;
+/* 0x51 */ char auto_scroll_state;
+/* 0x52 */ char did_first_pass;
+} WidgetTextAreaData_t;
+
+typedef struct WidgetTextArea { // 0x50
+/* 0x00 */ struct iFrame frame;
+/* 0x40 */ WidgetTextAreaData_t *data;
+/* 0x44 */ bool bTextSizesDirty;
+/* 0x48 */ float text_w;
+/* 0x4c */ float text_h;
+} WidgetTextArea_t;
+
+typedef struct Widget2D { // 0x20
+/* 0x00 */ short int *positions;
+/* 0x04 */ short int *uvs;
+/* 0x08 */ int *rgbas;
+/* 0x0c */ char *polys;
+/* 0x10 */ short int vert_cnt;
+/* 0x12 */ short int prim_cnt;
+/* 0x14 */ char prim_type;
+/* 0x15 */ char flags;
+/* 0x16 */ short int frame_cnt;
+/* 0x18 */ int ipad[2];
+} Widget2D_t;
+
+typedef struct Widget3D { // 0x3c
+/* 0x00 */  struct iGraphicsObject object;
+/* 0x28 */ vec4f *textureChannel;
+/* 0x2c */ vec4f *animationFrameChannel;
+/* 0x30 */ unsigned int geometryPtr;
+/* 0x34 */ struct HudWidget *widget3dData;
+/* 0x38 */ bool bExternalTextureChannel;
+/* 0x39 */ bool bExternalAnimationChannel;
+/* 0x3a */ bool bEnableSplitScreenHack;
+} Widget3D_t;
+
+typedef struct WidgetSprite { // 0x30
+/* 0x00 */  struct iGraphicsObject object;
+/* 0x28 */ vec4f *textureChannel;
+/* 0x2c */ bool bExternalTextureChannel;
+} WidgetSprite_t;
+
+typedef struct WidgetTextGfx { // 0x40
+/* 0x00 */ struct iGraphicsObject object;
+/* 0x28 */ struct fontLetter *fontTable;
+/* 0x30 */ u64 font_FX;
+/* 0x38 */ char *pExternalString;
+/* 0x3c */ TextAlignment_e textAlignment;
+} WidgetTextGfx_t;
+
+typedef struct WidgetSlider { // 0x3c
+/* 0x00 */  struct iGraphicsObject object;
+/* 0x28 */ bool hasOutline;
+/* 0x2c */ int outlineScale;
+/* 0x30 */ unsigned int maxScale;
+/* 0x34 */ unsigned int maxValue;
+/* 0x38 */ unsigned int sliderEmptyColor;
+} WidgetSlider_t;
+
+typedef struct WidgetWheel { // 0x54
+/* 0x00 */ struct iFrame frame;
+/* 0x40 */ vec2f rot;
+/* 0x48 */ void *presenter;
+/* 0x4c */ u32 color;
+/* 0x50 */ int textureId;
+} WidgetWheel_t;
 
 // returns pointer to radar map settings.
 HudMap_t *hudGetMapData(void);
