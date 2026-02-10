047aa13bf18840474556cd542e6f668a06822d76
Metroynome <yort124@gmail.com>
2026-02-09 22:40:08 -0500

    draw.c/.h now supported!

--- libuya/src/draw.c
+++ libuya/src/draw.c
@@ -1,5 +1,38 @@
-#include "draw.h"
+#include "common.h"
+#include "player.h"
+#include "graphics.h"
 
+typedef enum LineStyle {
+    LINE_STYLE_NONE = 0,
+    LINE_STYLE_BEAM = 1,
+    LINE_STYLE_SQUIGLY = 2,
+    LINE_STYLE_LIGHTNING = 3,
+    LINE_STYLE_NUM_STYLES = 4
+} LineStyle_e;
+
+typedef struct LineStatic {
+    /* 0x00 */ int numSegments;
+    /* 0x04 */ int texture;
+    /* 0x08 */ float textureOffset;
+    /* 0x0c */ float textureRepeatDistance;
+    /* 0x10 */ bool billboard;
+    /* 0x11 */ bool rotateTexture;
+    /* 0x12 */ char pad[2];
+    /* 0x14 */ float lineWidth;
+    /* 0x18 */ float widthStart;
+    /* 0x1c */ float widthEnd;
+    /* 0x20 */ int scrollFrames;
+    /* 0x24 */ float pointRandomOffset;
+} LineStatic_t;
+
+typedef struct LineEndPoint {
+    /* 0x00 */ VECTOR pos;
+    /* 0x10 */ int color;
+    /* 0x14 */ bool lerpColor;
+    /* 0x15 */ char pad[2];
+    /* 0x17 */ LineStyle_e style;  // Fixed: was LineStyles_e
+    /* 0x18 */ float fadeEnds;
+} LineEndPoint_t;
 
 LineStatic_t BeamLineStyle = {
     .numSegments = 2,
@@ -43,7 +76,7 @@ LineStatic_t LightningLineStyle = {
     .widthEnd = 0.1f,
 };
 
-LineStatic_t* LineStyles[LINE_STYLE_NUM_STYLES] = {
+LineStatic_t* LineStyles[] = {
     NULL,
     &BeamLineStyle,
     &SquiglyLineStyle,
