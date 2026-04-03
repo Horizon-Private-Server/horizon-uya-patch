118b60a04e9dcf237e5c244468e8ecf21cf05f66
Metroynome <yort124@gmail.com>
2026-04-03 12:02:23 -0400

    added strcpy

--- libuya/src/string.c
+++ libuya/src/string.c
@@ -7,3 +7,16 @@ int strlen(const char * str)
     while (*p) p++;
     return p - str;
 }
+
+int strcpy(char *dest, const char *src, int n)
+{
+    int i = 0;
+    if (!dest || !src) return 0;
+    if (n == 0) return 0;
+
+    for (i = 0; i < n - 1 && src[i] != '\0'; ++i) {
+        dest[i] = src[i];
+    }
+    dest[i] = '\0';
+    return i;
+}
